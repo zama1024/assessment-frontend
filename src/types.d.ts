a52d7f560c4acc097fb94addf3c7d68a6a2a43a7
Farshid Zaman <farshidzaman@Farshids-MacBook-Pro-2.local>
2023-02-12 17:31:20 -0500

    cleanup

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,48 +1,48 @@
 export type Variable = {
-    name: string,
-    type: string,
-    initialValue: string,
-}
+    name: string;
+    type: string;
+    initialValue: string;
+};
 
 export type List = {
-    id: number,
-    components: Array<number>,
-}
+    id: number;
+    components: Array<number>;
+};
 
 export type Component = {
-    id: number,
-    type: string,
-    options: any,
-    children?: number
-}
+    id: number;
+    type: string;
+    options: any;
+    children?: number;
+};
 
 export type PageData = {
-    variables?: Array<Variable>,
-    lists: Array<List>,
-    components: Array<Component>,
-}
+    variables?: Array<Variable>;
+    lists: Array<List>;
+    components: Array<Component>;
+};
 
 export type SingleUpcomingWeather = {
-    day: string,
-    condition: string,
-    conditionName: string,
-}
+    day: string;
+    condition: string;
+    conditionName: string;
+};
 
 export type WeatherData = {
-    condition: string,
-    conditionName: string,
-    temperature: number | null,
-    unit: string,
-    location: string,
-    upcomming: Array<SingleUpcomingWeather>
-}
+    condition: string;
+    conditionName: string;
+    temperature: number | null;
+    unit: string;
+    location: string;
+    upcomming: Array<SingleUpcomingWeather>;
+};
 
 export type Condition = {
-    id: number,
-    type: string,
+    id: number;
+    type: string;
     options: {
-        variable: string,
-        value: string,
-    },
-    children: number,
-}
+        variable: string;
+        value: string;
+    };
+    children: number;
+};
